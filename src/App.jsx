48a722588c3f7e121b4c0057bd704a8d90e34990
noeflores <fgnoee@gmail.com>
2025-08-23 10:31:43 -0600

    add basic simulator

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import Simulator from './Simulator'
+import AddSimulatorCard from './AddSimulatorCard'
+import useAppStore from './appStore'
 
 function App() {
+  const { simulators, floorCount, setFloorCount, moveElevator, addPerson } = useAppStore()
 
   return (
     <div className="App">
-      Hola
+      <div className="controls">
+        <div>
+          <label>Floors: {floorCount}</label>
+          <input 
+            type="range" 
+            min="2" 
+            max="10" 
+            value={floorCount} 
+            onChange={(e) => setFloorCount(parseInt(e.target.value))}
+          />
+        </div>
+        <button onClick={moveElevator}>Move Elevator</button>
+        <button onClick={addPerson}>Add Person</button>
+      </div>
+      <div className="simulators-container">
+        {simulators.map(simulator => (
+          <Simulator key={simulator.id} simulatorId={simulator.id} />
+        ))}
+        {simulators.length < 3 && <AddSimulatorCard />}
+      </div>
     </div>
   )
 }
